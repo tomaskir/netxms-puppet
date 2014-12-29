e2eb65cc110754fe145baf140cb8b3e20afb56e5
tomaskir <tomas@atris.sk>
2014-12-29 12:20:54 +0100

    apt::netxms_repo  - specify file ownership  - apt update after repo changed/added

--- modules/apt/manifests/netxms_repo.pp
+++ modules/apt/manifests/netxms_repo.pp
@@ -1,6 +1,14 @@
 class apt::netxms_repo {
 	file { '/etc/apt/sources.list.d/netxms.list':
 		source	=> 'puppet:///modules/apt/netxms.list',
+		owner	=> 'root',
+		group	=> 'root',
 		mode	=> '0644',
+		notify	=> Exec['netxms - apt-get update']
+	}
+	
+	exec { 'netxms - apt-get update':
+		refreshonly	=> true,
+		command		=> "/usr/bin/apt-get update",
 	}
 }

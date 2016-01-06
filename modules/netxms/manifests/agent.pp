91c02c45a15cd2bdaedb43af80c773fc78921fa0
tomaskir <tomas@atris.sk>
2016-01-06 20:26:48 +0100

    'netxms::agent' class: - updates for changes in 2.0; - more class parameters to config the agent available; - class manages the nxagent FileStore directory;

--- modules/netxms/manifests/agent.pp
+++ modules/netxms/manifests/agent.pp
@@ -2,29 +2,33 @@ class netxms::agent (
   $ensure               = 'installed',
   $nxagentd_masters,
   $nxagentd_access_pwd,
-  $nxagentd_logfile     = '/var/log/nxagentd.log',
+  $nxagentd_logfile     = '/var/log/nxagentd',
   $nxagentd_proxy_agent = 'no',
   $nxagentd_proxy_snmp  = 'no',
+  $nxagentd_file_store  = '/var/nxagentd',
   $f_owner              = 'root',
   $f_group              = 'root',
   $f_mode               = '0600',
+  $d_mode               = '0700',
   $repo_class           = 'apt::netxms_repo',
 )
 {
+  if ($nxagentd_masters == '' or $nxagentd_access_pwd == '') {
+    alert('Alert: netxms::agent class was declared, but some attributes were passed to it as empty strings.')
+  }
+
   # install packages
   package { 'netxms-base':
     require         => Class[$repo_class],
     ensure          => $ensure,
-    install_options => '--force-yes',
   }
 
   package { 'netxms-agent':
     require         => Package['netxms-base'],
     ensure          => $ensure,
-    install_options => '--force-yes',
   }
 
-  # config files
+  # config file
   file { '/etc/nxagentd.conf':
     require => Package['netxms-agent'],
     content => template('netxms/nxagentd.conf.erb'),
@@ -34,9 +38,18 @@ class netxms::agent (
     notify  => Service['nxagentd'],
   }
 
-  # services
+  # FileStore directory
+  file { $nxagentd_file_store:
+    require => Package['netxms-agent'],
+    ensure  => 'directory',
+    owner   => $f_owner,
+    group   => $f_group,
+    mode    => $d_mode,
+    notify  => Service['nxagentd'],
+  }
+
+  # service
   service { 'nxagentd':
-    require => File['/etc/nxagentd.conf'],
-    ensure  => 'running',
+    ensure => 'running',
   }
 }

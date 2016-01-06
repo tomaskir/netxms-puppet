e88724497daf8dab38e097d9fb8e64b019044696
tomaskir <tomas@atris.sk>
2016-01-06 20:23:53 +0100

    'apt::netxms_repo' class: - added error handling for unsupported OSes; - added apt repo addition by distribution; - fixed apt repo key addition;

--- modules/apt/manifests/netxms_repo.pp
+++ modules/apt/manifests/netxms_repo.pp
@@ -2,22 +2,41 @@ class apt::netxms_repo (
   $release = 'main',
 )
 {
+  $os_name = $lsbdistid ? {
+    'Ubuntu' => 'ubuntu',
+    'Debian' => 'debian',
+    default  => undef,
+  }
+
+  if $os_name == undef {
+    fail('Unsupported OS - this module currently only supports Debian and Ubuntu!')
+  }
+
+  $os_dist = $lsbdistcodename ? {
+    'trusty'  => 'trusty',
+    'jessie'  => 'jessie',
+    'lenny'   => 'lenny',
+    'squeeze' => 'squeeze',
+    default   => 'wheezy',
+  }
+
   file { '/etc/apt/sources.list.d/netxms.list':
     content => template('apt/netxms.list.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    notify  => Exec['netxms - add repo key']
+    notify  => Exec['netxms - add apt repo key']
   }
 
-  exec { 'netxms - add repo key':
+  exec { 'netxms - add apt repo key':
+    require     => File['/etc/apt/sources.list.d/netxms.list'],
+    command     => '/usr/bin/curl http://packages.netxms.org/netxms.gpg | /usr/bin/apt-key add -',
     refreshonly => true,
-    command     => '/usr/bin/wget -q -O - http://packages.netxms.org/netxms.gpg | sudo apt-key add -',
     notify      => Exec['netxms - apt-get update']
   }
 
   exec { 'netxms - apt-get update':
-    refreshonly => true,
     command     => '/usr/bin/apt-get update',
+    refreshonly => true,
   }
 }
